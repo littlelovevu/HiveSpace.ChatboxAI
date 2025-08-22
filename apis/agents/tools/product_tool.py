e34b0abee44302f5d36ba377a8ab90414f18f35f
nguyenphuctinh <nguyenphuctinh>
2025-08-22 11:39:04 +0700

    update

--- apis/agents/tools/product_tool.py
+++ apis/agents/tools/product_tool.py
@@ -9,8 +9,8 @@ from pydantic import BaseModel, Field
 class ProductSearchInput(BaseModel):
     input:str = Field(description="Nội dung cần tìm kiếm về thông tin sản phẩm trong cơ sở dữ liệu để cập nhật thêm thông tin trả lời.")
 
-@tool("web_search", args_schema=ProductSearchInput, return_direct=True)
-def web_search(input: str):
+@tool("product_search", args_schema=ProductSearchInput, return_direct=True)
+def product_search(input: str):
     """
     Tìm kiếm thông tin sản phẩm trong cơ sở dữ liệu  dựa vào nội dung người dùng cung cấp.
     """
